ac0243d5f6eeb5de8ed38f65948ea968e7431a86
Sonny Garcia <sonnygarcia@icloud.com>
2019-07-08 11:22:51 -0400

    adds tests for PluginInstall and refactors mocking

--- wrapper_test.go
+++ wrapper_test.go
@@ -5,7 +5,6 @@ import (
 	"io/ioutil"
 	"testing"
 
-	"github.com/dominodatalab/vagrant-exec/command"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -24,23 +23,26 @@ func (m *mockRunner) Execute(cmd string, cmdargs ...string) ([]byte, error) {
 	return nil, args.Error(1)
 }
 
-func newTestWrapper(runner command.Runner) wrapper {
-	logger := logrus.New()
-	logger.Out = ioutil.Discard
+// mockedWrapperFn returns a generator func that creates a wrapper with a mocked runnner. The func expects the output
+// and error values that the runner will return when invoked.
+func mockedWrapperFn(runnerArgs []string) func([]byte, error) wrapper {
+	return func(out []byte, err error) wrapper {
+		runner := new(mockRunner)
+		runner.On("Execute", "vagrant", runnerArgs).Return(out, err)
 
-	return wrapper{
-		executable: binary,
-		logger:     logger,
-		runner:     runner,
+		logger := logrus.New()
+		logger.Out = ioutil.Discard
+
+		return wrapper{
+			executable: binary,
+			logger:     logger,
+			runner:     runner,
+		}
 	}
 }
 
 func TestUp(t *testing.T) {
-	mockUp := func(out []byte, err error) wrapper {
-		runner := new(mockRunner)
-		runner.On("Execute", "vagrant", []string{"up"}).Return(out, err)
-		return newTestWrapper(runner)
-	}
+	mockUp := mockedWrapperFn([]string{"up"})
 
 	t.Run("success", func(t *testing.T) {
 		w := mockUp([]byte("up output"), nil)
@@ -54,11 +56,7 @@ func TestUp(t *testing.T) {
 }
 
 func TestHalt(t *testing.T) {
-	mockHalt := func(out []byte, err error) wrapper {
-		runner := new(mockRunner)
-		runner.On("Execute", "vagrant", []string{"halt"}).Return(out, err)
-		return newTestWrapper(runner)
-	}
+	mockHalt := mockedWrapperFn([]string{"halt"})
 
 	t.Run("success", func(t *testing.T) {
 		w := mockHalt([]byte("halt output"), nil)
@@ -72,11 +70,7 @@ func TestHalt(t *testing.T) {
 }
 
 func TestDestroy(t *testing.T) {
-	mockDestroy := func(out []byte, err error) wrapper {
-		runner := new(mockRunner)
-		runner.On("Execute", "vagrant", []string{"destroy", "--force"}).Return(out, err)
-		return newTestWrapper(runner)
-	}
+	mockDestroy := mockedWrapperFn([]string{"destroy", "--force"})
 
 	t.Run("success", func(t *testing.T) {
 		w := mockDestroy([]byte("destroy output"), nil)
@@ -90,11 +84,7 @@ func TestDestroy(t *testing.T) {
 }
 
 func TestStatus(t *testing.T) {
-	mockStatus := func(out []byte, err error) wrapper {
-		runner := new(mockRunner)
-		runner.On("Execute", "vagrant", []string{"status", "--machine-readable"}).Return(out, err)
-		return newTestWrapper(runner)
-	}
+	mockStatus := mockedWrapperFn([]string{"status", "--machine-readable"})
 
 	t.Run("one_machine", func(t *testing.T) {
 		w := mockStatus(ioutil.ReadFile("testdata/status-single"))
@@ -142,11 +132,7 @@ func TestStatus(t *testing.T) {
 }
 
 func TestVersion(t *testing.T) {
-	mockVersion := func(resp []byte, err error) wrapper {
-		runner := new(mockRunner)
-		runner.On("Execute", "vagrant", []string{"version", "--machine-readable"}).Return(resp, err)
-		return newTestWrapper(runner)
-	}
+	mockVersion := mockedWrapperFn([]string{"version", "--machine-readable"})
 
 	t.Run("success", func(t *testing.T) {
 		w := mockVersion(ioutil.ReadFile("testdata/version"))
@@ -174,12 +160,7 @@ func TestVersion(t *testing.T) {
 
 func TestSSH(t *testing.T) {
 	sshCmd := "my-command 1 2 3"
-
-	mockSSH := func(resp []byte, err error) wrapper {
-		runner := new(mockRunner)
-		runner.On("Execute", "vagrant", []string{"ssh", "--no-tty", "--command", sshCmd}).Return(resp, err)
-		return newTestWrapper(runner)
-	}
+	mockSSH := mockedWrapperFn([]string{"ssh", "--no-tty", "--command", sshCmd})
 
 	t.Run("success", func(t *testing.T) {
 		w := mockSSH([]byte("command output"), nil)
@@ -198,11 +179,7 @@ func TestSSH(t *testing.T) {
 }
 
 func TestPluginList(t *testing.T) {
-	mockPluginList := func(resp []byte, err error) wrapper {
-		runner := new(mockRunner)
-		runner.On("Execute", "vagrant", []string{"plugin", "list", "--machine-readable"}).Return(resp, err)
-		return newTestWrapper(runner)
-	}
+	mockPluginList := mockedWrapperFn([]string{"plugin", "list", "--machine-readable"})
 
 	t.Run("success", func(t *testing.T) {
 		w := mockPluginList(ioutil.ReadFile("testdata/plugin-list"))
@@ -234,10 +211,43 @@ func TestPluginList(t *testing.T) {
 }
 
 func TestPluginInstall(t *testing.T) {
-	//mockPluginList := func(resp []byte, err error) {}
+	mockPluginList := mockedWrapperFn([]string{"plugin", "install", "my-plugin"})
 
-	// no name
-	// version
-	// local
-	// error
+	plugin := Plugin{Name: "my-plugin"}
+	t.Run("success", func(t *testing.T) {
+		wrapper := mockPluginList(nil, nil)
+
+		assert.NoError(t, wrapper.PluginInstall(plugin))
+	})
+
+	t.Run("error", func(t *testing.T) {
+		wrapper := mockPluginList(nil, errors.New("runner error"))
+
+		assert.Error(t, wrapper.PluginInstall(plugin))
+	})
+
+	t.Run("no_name", func(t *testing.T) {
+		plugin := Plugin{}
+		wrapper := mockPluginList(nil, nil)
+
+		err := wrapper.PluginInstall(plugin)
+		require.Error(t, err)
+		assert.Contains(t, err.Error(), "plugin must have a name")
+	})
+
+	t.Run("with_version", func(t *testing.T) {
+		mockPluginList := mockedWrapperFn([]string{"plugin", "install", "my-plugin", "--plugin-version", "1.2.3"})
+		plugin := Plugin{Name: "my-plugin", Version: "1.2.3"}
+		wrapper := mockPluginList(nil, nil)
+
+		assert.NoError(t, wrapper.PluginInstall(plugin))
+	})
+
+	t.Run("local_install", func(t *testing.T) {
+		mockPluginList := mockedWrapperFn([]string{"plugin", "install", "my-plugin", "--local"})
+		plugin := Plugin{Name: "my-plugin", Location: "local"}
+		wrapper := mockPluginList(nil, nil)
+
+		assert.NoError(t, wrapper.PluginInstall(plugin))
+	})
 }
